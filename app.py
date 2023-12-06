99ea383964c1821639d108949470f7191d07d9ab
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-12-06 12:33:10 -0800

    started models and app files

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Users
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +14,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def list_users():
+    """List users and show add form."""
+
+    users = Users.query.all()
+    return render_template("list.html", users=users)
--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class Users(db.Model):
+    """Users table."""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    first_name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    last_name = db.Column(
+        db.String(100))
