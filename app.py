d6e473899d275247550ccbf9ef6cc1a93f1a5002
Carl Molina <carlandrew.molina@gmail.com>
2023-12-06 16:35:42 -0800

    Finished user-detail.html

--- app.py
+++ app.py
@@ -35,3 +35,28 @@ def show_all_users():
         'user-listing.html',
         users=users
     )
+
+@app.get("/users/new")
+def show_add_form():
+    """Show an add form for users"""
+
+    return render_template(
+        'new-user-form.html'
+    )
+
+@app.get("/users/<int:user_id>")
+def show_user_id_information(user_id):
+    """Show information about the given user"""
+
+    user = Users.query.get_or_404(user_id)
+
+    return render_template('user-detail.html', user=user)
+
+@app.get("/users/<int:user_id>/edit")
+def show_edit_page(user_id):
+    """Show the edit page for a user"""
+
+    user = Users.query.get_or_404(user_id)
+
+    return render_template('edit-user.html', user=user)
+
